5a25b1080e3b815f21c59c64cfad856ceac20f34
Katsusuke Shimizu <katsusuke@gmail.com>
2017-05-13 15:00:38 +0900

    show twitter limeline into home

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -9,27 +9,38 @@ class Feed
   def get(limit, max_id = nil, since_id = nil)
     max_id     = '+inf' if max_id.blank?
     since_id   = '-inf' if since_id.blank?
-    unhydrated = redis.zrevrangebyscore(key, "(#{max_id}", "(#{since_id}", limit: [0, limit], with_scores: true).map(&:last).map(&:to_i)
-    client = @account.twitter_accounts.first&.client
-    if client && Time.zone.now + 3.minutes < client.last_updated_at
-      app = Doorkeeper::Application.first
-      client.home_timeline.each do |tweet|
-        user = tweet.user
-        account = Account.find_or_create_by(domain: 'twitter.com', username: user.screen_name) do |a|
-          a.display_name = user.name
-          a.note = user.description
-          a.url = "https://twitter.com/#{user.screen_name}"
-          a.avatar_remote_url = user.profile_image_uri_https(:bigger).to_s
-        end
-        Status.find_or_create_by(url: tweet.url.to_s) do |s|
-          s.account = account
-          s.visibility = :private
-          s.text = tweet.text
-          s.application = app
+    if twitter_account = @account.twitter_accounts.first
+      if twitter_account.last_updated_at + 3.minutes < Time.zone.now
+        if client = twitter_account.client
+          app = Doorkeeper::Application.first
+          accounts = []
+          client.home_timeline.each do |tweet|
+            user = tweet.user
+
+            account = Account.find_or_create_by(domain: 'twitter.com', username: user.screen_name) do |a|
+              a.display_name = user.name
+              a.note = user.description || ' '
+              a.url = "https://twitter.com/#{user.screen_name}"
+              a.avatar_remote_url = user.profile_image_uri_https(:bigger).to_s
+            end
+            accounts << account
+            status = Status.find_or_initialize_by(url: tweet.url.to_s) do |s|
+              s.account = account
+              s.visibility = :private
+              s.text = tweet.text
+              s.application = app
+            end
+            redis.zadd(key, status.id, status.id) if status.new_record? && status.save
+          end
+          accounts.each do |account|
+            @account.active_relationships.find_or_create_by(target_account: account)
+          end
+          twitter_account.update(last_updated_at: Time.zone.now)
         end
       end
-      @account.twitter_accounts.first.update(last_updated_at: Time.zone.now)
     end
+
+    unhydrated = redis.zrevrangebyscore(key, "(#{max_id}", "(#{since_id}", limit: [0, limit], with_scores: true).map(&:last).map(&:to_i)
 
     status_map = Status.where(id: unhydrated).cache_ids.map { |s| [s.id, s] }.to_h
     unhydrated.map { |id| status_map[id] }.compact
